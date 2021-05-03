d76f196e97d68777971718437e1c5ed7de887d7f
hayles-img <57105041+hayles-img@users.noreply.github.com>
2021-05-03 00:15:06 -0400

    added pipeline

---
+++ utils/make_pipeline.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.compose import ColumnTransformer 
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+
+def pre_processing(numerical_columns,categorical_columns):
+    numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())])
+
+    categorical_transformer = OneHotEncoder(handle_unknown='ignore')
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numeric_transformer, numerical_columns),
+            ('cat', categorical_transformer, categorical_columns)])
+    
+
+def make_model_pipeline(numerical_columns,categorical_columns):
+    preprocessor = pre_processing(numerical_columns,categorical_columns)
+    model_pipeline = Pipeline(steps=[('pre_processing',preprocessor),('classifer',LogisticRegression())])
+    return model_pipeline
