20823c92ec1aba78b7f5d0a748bfc4a7a8cc1f1d
amauris <d1at196@yahoo.com>
2021-05-03 16:20:03 -0400

    Aggregate merged bureau on SK_ID_CURR to be able to join with application_df

--- utils/data_extract_utils.py
+++ utils/data_extract_utils.py
@@ -12,8 +12,7 @@ def extract_features_from_bureau(bureau_df, bureau_balances_df):
     bureau_df['AMT_CREDIT_SUM_DEBT'] = bureau_df['AMT_CREDIT_SUM_DEBT'].fillna(value=0)
     bureau_df['AMT_CREDIT_DEBT_RATIO'] =  bureau_df['AMT_CREDIT_SUM_DEBT']/bureau_df['AMT_CREDIT_SUM']
     bureau_df['AMT_CREDIT_DEBT_RATIO'] = bureau_df['AMT_CREDIT_DEBT_RATIO'].fillna(value=0)
-    
-    
+
     bureau_df = bureau_df.set_index('SK_ID_BUREAU')
     
     DPD_STATUS_MAP = {
@@ -35,6 +34,9 @@ def extract_features_from_bureau(bureau_df, bureau_balances_df):
     dpd_counts_df = bureau_balances_df.groupby(['SK_ID_BUREAU']).apply(sum_of_dpd)
     dpd_counts_df = pd.DataFrame(dpd_counts_df, columns=['DPD_COUNTS']);
     bureau_with_dpds = pd.concat([bureau_df, dpd_counts_df], axis=1)
-    bureau_with_dpds = bureau_with_dpds.set_index('SK_ID_CURR')
     
-    return bureau_with_dpds[:][['AMT_CREDIT_DEBT_RATIO', 'CREDIT_DAY_OVERDUE', 'DPD_COUNTS']]
+    bureau_with_dpds = bureau_with_dpds[:][['SK_ID_CURR', 'AMT_CREDIT_DEBT_RATIO', 'CREDIT_DAY_OVERDUE', 'DPD_COUNTS']]
+    
+    # Further aggregation to make sure unique SK_ID_CURR are returned
+    bureau_with_dpds = bureau_with_dpds.groupby(['SK_ID_CURR']).mean()
+    return bureau_with_dpds
